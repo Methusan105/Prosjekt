aa49a34e82b1fc090c7c38166ca61fca8ce4768b
Methusan105 <methusan@hotmail.no>
2023-05-25 09:46:02 +0200

    12

--- Datasett.py
+++ Datasett.py
@@ -5,8 +5,8 @@ url="https://raw.githubusercontent.com/Methusan105/Prosjekt/main/co2_annmean_gl.
 df= pd.read_csv(url,index_col=0)
 df 
 
-x = df["Year"] # x er en liste med datoer
-y = df["Gjennomsnitt"] # y er en liste med priser
+x = df["year"] # x er en liste med datoer
+y = df["gjennomsnitt"] # y er en liste med priser
 plt.plot(x, y)
 plt.xlabel("År")
 plt.ylabel("Pris")
