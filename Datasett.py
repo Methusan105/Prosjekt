0e455ec5ca7da679ad38b5a5494c3ed8959648cf
Methusan105 <methusan@hotmail.no>
2023-05-25 09:58:11 +0200

    12

--- Datasett.py
+++ Datasett.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-url="https://raw.githubusercontent.com/Methusan105/Prosjekt/main/co2_annmean_gl.csv"
+url="https://raw.githubusercontent.com/Methusan105/Prosjekt/main/Data.csv"
 
 df= pd.read_csv(url,index_col=0)
 df 
 
-x = df["Year"] # x er en liste med datoer
-y = df["Gjennomsnitt"] # y er en liste med priser
+x = df["year"] # x er en liste med datoer
+y = df["gjennomsnitt"] # y er en liste med priser
 plt.plot(x, y)
 plt.xlabel("År")
 plt.ylabel("Gjennomsnitt")
