40b6e35132092c9bc3157d06d7cd1ac45f0f163d
Methusan105 <methusan@hotmail.no>
2023-05-26 11:05:12 +0200

    12

--- Datasett.py
+++ Datasett.py
@@ -1,5 +1,7 @@
+from pylab import *
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import *
 url="https://raw.githubusercontent.com/Methusan105/Prosjekt/main/Data.csv"
 
 df= pd.read_csv(url,index_col=0)
@@ -11,3 +13,5 @@ plt.plot(x, y)
 plt.xlabel("År")
 plt.ylabel("Gjennomsnitt")
 plt.show()
+
+""" Hvordan har CO2-nivåene endret seg over tid, og hva kan dette fortelle oss om klimaendringer? """
