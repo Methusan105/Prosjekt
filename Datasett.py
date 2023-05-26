8cda3d8c48130be5a142e352b884657c2ce1aa3c
Methusan105 <methusan@hotmail.no>
2023-05-26 11:21:20 +0200

    12

--- Datasett.py
+++ Datasett.py
@@ -2,16 +2,27 @@ from pylab import *
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import *
-url="https://raw.githubusercontent.com/Methusan105/Prosjekt/main/Data.csv"
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
-df= pd.read_csv(url,index_col=0)
-df 
+url = "https://raw.githubusercontent.com/Methusan105/Prosjekt/main/Data.csv"
+df = pd.read_csv(url, index_col=0)
 
-x = df["year"] # x er en liste med datoer
-y = df["gjennomsnitt"] # y er en liste med priser
-plt.plot(x, y)
+x = df["year"]
+y = df["gjennomsnitt"]
+
+# Utfør lineær regresjon
+poly = PolynomialFeatures(degree=1)
+X = poly.fit_transform(np.array(x).reshape(-1, 1))
+model = LinearRegression()
+model.fit(X, y)
+y_pred = model.predict(X)
+
+# Plot data og regresjonslinje
+plt.scatter(x, y, label="Observasjoner")
+plt.plot(x, y_pred, color='red', label="Regresjonslinje")
 plt.xlabel("År")
 plt.ylabel("Gjennomsnitt")
+plt.legend()
 plt.show()
-
-""" Hvordan har CO2-nivåene endret seg over tid, og hva kan dette fortelle oss om klimaendringer? """
