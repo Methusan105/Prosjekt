cd6725348ae95b09a4f1dc0c43b81170bf04a14e
Methusan105 <methusan@hotmail.no>
2023-06-02 11:25:38 +0200

    12

--- Datasett.py
+++ Datasett.py
@@ -6,19 +6,20 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
+# Henter inn data fra url og leser den
 url = "https://raw.githubusercontent.com/Methusan105/Prosjekt/main/Data.csv"
 df = pd.read_csv(url, index_col=0)
 x = df["year"]
 y = df["gjennomsnitt"]
 
-# Utfør lineær regresjon
+# Utfører lineær regresjon
 poly = PolynomialFeatures(degree=2)
 X = poly.fit_transform(np.array(x).reshape(-1, 1))
 model = LinearRegression()
 model.fit(X, y)
 y_pred = model.predict(X)
 
-# Plot data og regresjonslinje
+# Plotter data og regresjonslinje
 plt.scatter(x, y, label="Observasjoner")
 plt.plot(x, y_pred, color='red', label="Regresjonslinje")
 plt.xlabel("År")
